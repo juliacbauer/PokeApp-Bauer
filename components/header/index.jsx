3d50543565e5b5166dcd13b21a88556cfb97ce77
Julia Bauer <juliabauer@ufl.edu>
2024-04-01 17:27:47 -0400

    create pages and search

---
+++ components/header/index.jsx
@@ -0,0 +1,40 @@
+import styles from "./style.module.css";
+import Link from "next/link";
+import useLogout from "../../hooks/useLogout";
+
+export default function Header(props) {
+  const logout = useLogout();
+  return (
+    <header className={styles.container}>
+      {props.isLoggedIn ? (
+        <>
+          <p>
+            <Link href="/">Home</Link>
+          </p>
+          <div className={styles.container}>
+            <p>Welcome, {props.username}!</p>
+            <p>
+            <Link href="/search">Search</Link>
+            </p>
+            <p>
+            <Link href="/favorites">Favorites</Link>
+            </p>
+            <p onClick={logout} style={{ cursor: "pointer" }}>
+              Logout
+            </p>
+          </div>
+        </>
+      ) : (
+        <>
+          <p>
+            <Link href="/">Home</Link>
+          </p>
+          <p>
+            <Link href="/login">Login</Link>
+          </p>
+        </>
+      )}
+    </header>
+  );
+}
+
