b2e27c41e96e82f9b14bd538d6741f94f6cfa2c8
Julia Bauer <juliabauer@ufl.edu>
2024-04-04 18:10:23 -0400

    renders word display component

--- components/header/WordDisplay/index.jsx
+++ components/header/WordDisplay/index.jsx
@@ -3,10 +3,24 @@ export default function WordDisplay({ word }) {
       <div key={word.id}>
         <h2>{word.word}</h2>
         <ul>
-          {word.meanings.map((meaning, index) => (
-            <li key={index}>{meaning.definition}</li>
-          ))}
-        </ul>
+        {word.meanings.map((meaning, index) => (
+          <li key={index}>
+            <p>Part of Speech: {meaning.partOfSpeech}</p>
+            <p>Definitions:</p>
+            <ul>
+              {meaning.definitions.map((definition, index) => (
+                <li key={index}>{JSON.stringify(definition.definition)}</li>
+              ))}
+            </ul>
+            {meaning.synonyms.length > 0 && (
+              <p>Synonyms: {meaning.synonyms.join(', ')}</p>
+            )}
+            {meaning.antonyms.length > 0 && (
+              <p>Antonyms: {meaning.antonyms.join(', ')}</p>
+            )}
+          </li>
+        ))}
+      </ul>
       </div>
     );
   }
