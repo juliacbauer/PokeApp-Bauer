1b62209dd8a7f67c55be09973a7af7ec01d396e3
Julia Bauer <juliabauer@ufl.edu>
2024-04-22 15:12:17 -0400

    prevent duplicate words

--- db/controllers/vocab.js
+++ db/controllers/vocab.js
@@ -15,14 +15,26 @@ export async function getVocab(userId) {
 //add word to vocab list
 export async function addToVocab(userId, word) {
   await dbConnect()
+  //check for user
+  const user = await User.findById(userId);
+  if (!user) {
+  return null
+  }
   //console.log(userId, word)
-  const user = await User.findByIdAndUpdate(
+  //prevent duplicate words
+  const existingWord = user.vocabularyList.find(
+    (vocabWord) => vocabWord.word === word.word
+  );
+  if (existingWord) {
+    console.log("Word already in vocab:", existingWord)
+    return false
+  }
+  await User.findByIdAndUpdate(
     userId,
     { $addToSet: { vocabularyList: word } },
     { new: true }
   )
-  if (!user) return null
-  return true
+  return word;
 }
 
 //remove word from vocab list
